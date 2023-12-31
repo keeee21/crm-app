16c3bf8cefe51b6befbb35d6f53b1a575b20020f
Keisuke Uemoto <keisuke.u211016@gmail.com>
2023-12-31 14:27:00 +0900

    inertiaとvue設定

---
+++ crm/resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <div>
+        <h1>Inertia Test</h1>
+        <a href="/">aタグ経由です</a><br><br>
+        <Link href="/">Link経由です</Link>
+        <br><br>
+        <Link :href="route('inertia.index')">名前付きルート</Link>
+    </div>
+</template>
